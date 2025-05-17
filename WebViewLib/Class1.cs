86fc4b57ec4d967e4905aed9c7ce81937fc349f9
Admin <Admin@LAPTOP-4HRM131V>
2025-05-17 21:59:04 +0300

    Add project files.

---
+++ WebViewLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebViewLib
+{
+    public class Class1
+    {
+    }
+}
